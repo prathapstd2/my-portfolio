9eb53a98345af03ebc16143a108ab6981e92cf54
prathapstd2@gmail.com <prathapstd2@gmail.com>
2025-01-27 00:12:16 +0530

    major update

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+document.querySelector('.hamburger-icon').addEventListener('click', function () {
+  const menuLinks = document.querySelector('.menu-links');
+  if (menuLinks.classList.contains('open')) {
+    menuLinks.style.zIndex = '1000';
+  } else {
+    menuLinks.style.zIndex = '0';
+  }
+});
